27d6a3c457c9466cb5dc4f7d1a48b6907449c0bb
Dulmi-2000 <dulmiwijenayaka@gmail.com>
2024-03-18 21:44:28 +0530

    navbar modified

--- src/layout/Navbar/Navbar.js
+++ src/layout/Navbar/Navbar.js
@@ -1,5 +1,104 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css';
+import { IoMdCloseCircle } from "react-icons/io";
+import { TbGridDots } from "react-icons/tb";
+
+export default function Navbar() {
+
+  const [active, setActive] = useState(false);
+
+  // Function to toggle navbar
+  const  shownav = () => {
+    setActive(!active);
+    // Toggles the state between true and false
+  };
+  
+  // Function to close nav bar
+  const closeNavbar = () => {
+    setActive(false);
+    // Set the state to false to close the navbar
+  };
+  
+
+
+  return (
+    <section className='NavBarSection'>
+      <header className='header flex'>
+
+        <div className='logodiv'>
+        <Link className="navbar-brand" to="/">Tourly</Link>
+       
+        </div>
+       
+
+        <div className={`navBar expand-lg ${active ? 'activeNavBar' : ''}`} >
+          
+          <ul className="navLists grid">
+            
+       
+          <li className="navitem">
+            <Link className="navlink" to="/">Home</Link>
+          </li>
+
+          <li className="navitem">
+            <Link className="navlink" to="Popular/Popular">Popular</Link>
+          </li>
+          <li className="navitem">
+            <Link className="navlink" to="Destination/Destination">Destination</Link>
+          </li>
+          <li className="navitem">
+            <Link className="navlink" to="Blog/Blog">Blog</Link>
+          </li>
+          
+            
+          <li className='navitem'>
+            <Link className="btn3" to="Login/Login"> Login  </Link>
+          </li>
+
+          <li className='navitem'>
+            <Link className="btn3"  to="Signup/Signup"> Sign Up  </Link>
+          </li>
+            
+          </ul> 
+
+
+          <div onClick={closeNavbar}  className="closeNavbar">
+            <IoMdCloseCircle className='close-icon'/>
+          </div>
+          
+
+        </div>
+  
+        <div className='togglenavbar expand-sm' onClick={shownav}>
+          <TbGridDots className='list-icon' />
+        </div>
+        
+
+      </header>
+
+    </section>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import "./navbar.css"
 
 export default function Navbar() {
@@ -24,24 +123,7 @@ export default function Navbar() {
       </button>
 
       <div className={`collapse navbar-collapse ${collapsed ? '' : 'show'}`}>
-     {/*
-        <div className='nav_bar'>
-          <div className='menu'>
-          <Link className="navbar-brand" to="/">Tourly</Link>
-                <Link className='nav-link' to="/">Home</Link>
-                <Link className="nav-link" to="Popular/Popular">Popular</Link>
-                <Link className="nav-link" to="Blog/Blog">Blog</Link>
-          </div>
-          
 
-            <div className='menu1'>
-            <Link className="btn" to="Login/Login"> Login  </Link>
-            <Link className="btn"  to="Signup/Signup"> Sign Up  </Link>
-            </div>
-
-            </div>
-            
-     */}
 
      
        <ul className="navbar-nav">
@@ -76,3 +158,4 @@ export default function Navbar() {
        
   );
 }
+*/
